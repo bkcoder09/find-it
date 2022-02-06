71bab84a9007ae1d85857005d7ffcc48ea7e7ab5
bkcoder09 <80070457+bkcoder09@users.noreply.github.com>
2022-02-06 14:17:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup(){
     canvas = createCanvas(350, 300);
@@ -15,10 +16,40 @@ function start(){
 }
 
 function modelLoaded(){
-    console.log(" English - The Model is Loaded ");
+    console.log(" The Model is Loaded ");
     status = true;
 }
 
 function draw(){
     image(video, 0, 0, 350, 300);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(text == hehe){
+                videoLiveView.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("ohno").innerHTML = hehe+" is Found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(hehe);
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("ohno").innerHTML = hehe+" is not Found";
+            }
+        }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
